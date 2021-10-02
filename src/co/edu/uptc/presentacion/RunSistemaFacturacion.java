63049a374990447c5a5ea24f28eae181b71a8516
UPTCSebastianBarrera <86314559+UPTCSebastianBarrera@users.noreply.github.com>
2021-10-02 09:03:30 -0500

    Se importo el GUIIndex

--- src/co/edu/uptc/presentacion/RunSistemaFacturacion.java
+++ src/co/edu/uptc/presentacion/RunSistemaFacturacion.java
@@ -1,12 +1,16 @@
 package co.edu.uptc.presentacion;
 
+import co.edu.uptc.presentacion.GUI.GUIindex;
+
 public class RunSistemaFacturacion {
 	
 	public static void main(String[] args) {
 		
-		MenuSistemaFacturacion menu = new MenuSistemaFacturacion();
+		//MenuSistemaFacturacion menu = new MenuSistemaFacturacion();
 		
-		menu.MenuConsola();
+		//menu.MenuConsola();
+		
+		new GUIindex().setVisible(true);
 		
 	}
 
